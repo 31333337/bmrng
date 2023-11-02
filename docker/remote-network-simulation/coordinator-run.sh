72e056655548a8c61cc5f8e1d7bb55f7c1105ed5
Hans Bricks <hans@dialout.net>
2023-11-02 02:15:18 +0000

    feat(docker/scripts): add multiple connections

--- docker/remote-network-simulation/coordinator-run.sh
+++ docker/remote-network-simulation/coordinator-run.sh
@@ -7,18 +7,19 @@ if [[ $(pwd) == "/com.docker.devenvironments.code/"* ]]; then
     BASE_DIR="/com.docker.devenvironments.code"
 fi
 
-args='--numservers 3 --numgroups 3 --numusers 10 --groupsize 3 --numlayers 10'
+# Fetch IP address of eth0
+ETH0_IP=$(ip addr show eth0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
+
+# Create 10 hosts on 10 different ports based on eth0 IP and append to hostsfile
+START_PORT=8000
+END_PORT=$((START_PORT + 9))
 hostsfile="$BASE_DIR/go/trellis/cmd/experiments/ip.list"
-echo -e 'server-0\nserver-1\nserver-2' > "${hostsfile}"
+> "${hostsfile}" # Emptying the file before appending
+for port in $(seq $START_PORT $END_PORT); do
+    echo "${ETH0_IP}:${port}" >> "${hostsfile}"
+done
+
+args='--numservers 3 --numgroups 3 --numusers 10 --groupsize 3 --numlayers 10'
 cd "$BASE_DIR/go/trellis/cmd/coordinator" && go build
 cd "$BASE_DIR/go/0kn/cmd/xtrellis" && go build && ./xtrellis coordinator config ${args} --hostsfile ${hostsfile}
 cd "$BASE_DIR/go/trellis/cmd/coordinator" && ./coordinator ${args} --runtype 2
-# create hosts file, generate config from it, launch coordinator using it
-
-#iargs='--numservers 3 --numgroups 3 --numusers 10 --groupsize 3 --numlayers 10'
-#hostsfile="$BASE_DIR/go/trellis/cmd/experiments/ip.list"
-
-#echo -e 'server-0\nserver-1\nserver-2' > "${hostsfile}"
-#cd /com.docker.devenvironments.code/go/trellis/cmd/coordinator && go build
-#cd /com.docker.devenvironments.code/go/0kn/cmd/xtrellis && go build && ./xtrellis coordinator config ${args} --hostsfile ${hostsfile}
-#cd /com.docker.devenvironments.code/go/trellis/cmd/coordinator && ./coordinator ${args} --runtype 2
